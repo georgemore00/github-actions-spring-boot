9fce8b8cfe4d16860708e2c11e9559c4a889739b
George moré <georgemore00@hotmail.com>
2023-03-19 22:04:19 +0100

    added sonarcloud scan step

--- src/main/java/com/george/githubactions/GithubActionsApplication.java
+++ src/main/java/com/george/githubactions/GithubActionsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GithubActionsApplication.class, args);
+  }
 }
--- src/main/java/com/george/githubactions/HelloWorldController.java
+++ src/main/java/com/george/githubactions/HelloWorldController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String helloWorld(){
-        return "Hello world!";
-    }
+  @GetMapping("/")
+  public String helloWorld() {
+    return "Hello world!";
+  }
 }
