9b170dffa3f5c19df89b569983fbdd8310f12cb5
George moré <georgemore00@hotmail.com>
2023-03-19 15:23:54 +0100

    Initial commit

---
+++ src/main/java/com/george/githubactions/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.george.githubactions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+}
